b568de79bb695ad36c27d0b5a7a5779193229fec
Ahmed Murtaza <ahmed.gmurtaza@hotmail.com>
2017-02-19 16:38:33 +0500

    19-fet-work

--- app/src/main/java/com/saylani/databindingsample/MainActivity.java
+++ app/src/main/java/com/saylani/databindingsample/MainActivity.java
@@ -1,10 +1,14 @@
 package com.saylani.databindingsample;
 
 import android.databinding.DataBindingUtil;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.saylani.databindingsample.databinding.MyActivityBinding;
 
@@ -19,8 +23,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
 
-        binding.myRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        setSupportActionBar(binding.toolbar);
+
+
+        binding.simpleToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Simple Toast",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        binding.materialToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(binding.materialToast.getRootView(),"Deleted Successfully",Snackbar.LENGTH_SHORT)
+                        .setAction("UNDO", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Toast.makeText(MainActivity.this,"Restored",Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .show();
+            }
+        });
+
+        binding.myRecyclerView.setLayoutManager(new GridLayoutManager(this,2));
         binding.myRecyclerView.setAdapter(new MyRecycleAdapter(this, data));
     }
+
+
 
 }
